c3f63094d131972ad21c43a69401e365646eebcc
postables <postables@rtradetechnologies.com>
2020-03-30 00:03:12 -0700

    cleanup deps

--- converter_test.go
+++ converter_test.go
@@ -12,24 +12,36 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
-func getSamples(t *testing.T) []string {
-	files, err := ioutil.ReadDir("samples")
+var (
+	listenAddress string
+)
+
+func init() {
+	listenAddress = os.Getenv("LISTEN_ADDRESS")
+	if listenAddress == "" {
+		listenAddress = "xapi.temporal.cloud:9090"
+	}
+}
+
+func getSamples(t *testing.T, dir string) []string {
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		t.Fatal(err)
 	}
 	fs := []string{}
 	for _, fh := range files {
 		if !fh.IsDir() {
-			fs = append(fs, "samples/"+fh.Name())
+			fs = append(fs, dir+"/"+fh.Name())
 		}
 	}
 	return fs
 }
-func TestConverter(t *testing.T) {
+
+func TestConverterGenerated(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	cl, err := client.NewClient(client.Opts{
-		ListenAddress: "xapi.temporal.cloud:9090",
+		ListenAddress: listenAddress,
 		Insecure:      true,
 	})
 	defer cl.Close()
@@ -37,7 +49,66 @@ func TestConverter(t *testing.T) {
 		t.Fatal(err)
 	}
 	converter := NewConverter(ctx, cl)
-	files := getSamples(t)
+	files := getSamples(t, "samples/generated")
+	for _, file := range files {
+		func() {
+			fh, err := os.Open(file)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer fh.Close()
+			data, err := ioutil.ReadAll(fh)
+			if err != nil {
+				t.Fatal(err)
+			}
+			email1, err := converter.Convert(bytes.NewReader(append(data[0:0:0], data...)))
+			if err != nil {
+				t.Fatal(err)
+			}
+			hash, err := converter.PutEmail(email1)
+			if err != nil {
+				t.Fatal(err)
+			}
+			email2, err := converter.GetEmail(hash)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !proto.Equal(email1, email2) {
+				t.Fatal("not equal")
+			}
+			email2Copy, err := converter.GetEmail(hash)
+			if err != nil {
+				t.Fatal(err)
+			}
+			chunkHash, err := converter.PutEmailChunked(email2)
+			if err != nil {
+				t.Fatal(err)
+			}
+			email3, err := converter.GetEmailChunked(chunkHash)
+			if err != nil {
+				t.Fatal(err)
+			}
+			// tests equality between dedicated ipld format, and unixfs format
+			if !proto.Equal(email2Copy, email3) {
+				t.Fatal("invalid email")
+			}
+		}()
+	}
+}
+
+func TestConverter(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	cl, err := client.NewClient(client.Opts{
+		ListenAddress: listenAddress,
+		Insecure:      true,
+	})
+	defer cl.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	converter := NewConverter(ctx, cl)
+	files := getSamples(t, "samples")
 	for _, file := range files {
 		func() {
 			fh, err := os.Open(file)
@@ -86,17 +157,18 @@ func TestConverter(t *testing.T) {
 }
 
 func TestChunkSizeCalc(t *testing.T) {
+	t.Skip("update not yet deplyoed to public temporalx instance")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	cl, err := client.NewClient(client.Opts{
-		ListenAddress: "localhost:9090",
+		ListenAddress: listenAddress,
 		Insecure:      true,
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
 	converter := NewConverter(ctx, cl)
-	files := getSamples(t)
+	files := getSamples(t, "samples")
 	var hashes = make([]string, len(files))
 	for i, file := range files {
 		fh, err := os.Open(file)
@@ -118,35 +190,6 @@ func TestChunkSizeCalc(t *testing.T) {
 		}
 		hashes[i] = hash
 	}
-	/*	for _, hash := range hashes {
-			ch, err := converter.GetChunkedEmail(hash)
-			if err != nil {
-				t.Fatal(err)
-			}
-			resp, err := cl.Blockstore(ctx, &pb.BlockstoreRequest{
-				RequestType: pb.BSREQTYPE_BS_GET_STATS,
-				Cids:        []string{hash},
-			})
-			if err != nil {
-				t.Fatal(err)
-			}
-			for _, block := range resp.GetBlocks() {
-				totalSize += block.GetSize_()
-			}
-			for _, chash := range ch.GetParts() {
-				resp, err := cl.Blockstore(ctx, &pb.BlockstoreRequest{
-					RequestType: pb.BSREQTYPE_BS_GET_STATS,
-					Cids:        []string{chash},
-				})
-				if err != nil {
-					t.Fatal(err)
-				}
-				for _, block := range resp.GetBlocks() {
-					totalSize += block.GetSize_()
-				}
-			}
-		}
-	*/
 	size, err := converter.CalculateChunkedEmailSize(hashes...)
 	if err != nil {
 		t.Fatal(err)
@@ -155,10 +198,11 @@ func TestChunkSizeCalc(t *testing.T) {
 }
 
 func TestNonChunkSizeCalc(t *testing.T) {
+	t.Skip("update not yet deplyoed to public temporalx instance")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	cl, err := client.NewClient(client.Opts{
-		ListenAddress: "localhost:9090",
+		ListenAddress: listenAddress,
 		Insecure:      true,
 	})
 	defer cl.Close()
@@ -166,7 +210,7 @@ func TestNonChunkSizeCalc(t *testing.T) {
 		t.Fatal(err)
 	}
 	converter := NewConverter(ctx, cl)
-	files := getSamples(t)
+	files := getSamples(t, "samples")
 	var hashes []string
 	var foundHashes = make(map[string]bool)
 	for _, file := range files {
